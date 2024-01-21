72639c93909a9671e06d8a11b390eecaa6553761
Harrison Stephan <harrisonstephan@Harrisons-iMac.local>
2024-01-21 16:21:35 -0700

    added comments and the second choice function, will fix the async now

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ function ClassStats(characterName, type) {
     this.strength = 0;
     this.health = 0;
     this.money = 20;
+    this.experience = 0;
 };
 
+// this defines the starting values that the character has 
 ClassStats.prototype.startingValues = function () {
     if (this.type === "Warrior") {
         this.strength = 20;
@@ -24,11 +26,13 @@ ClassStats.prototype.startingValues = function () {
     }
 };
 
+// this is a prototype function that displays in the console the basic stats of the character. 
 ClassStats.prototype.introduction = function () {
     console.log(`Hello ${this.characterName}! Your class is: ${this.type}. You have ${this.health} health and a strength of ${this.strength}. Your starting money is ${this.money}`);
 };
 
-ClassStats.prototype.listChoice = function (variableName, text, option1, option2, option3) {
+// this is a reusable function that creates a list based on 3 given options
+ClassStats.prototype.threeListChoice = function (variableName, text, option1, option2, option3) {
    return new Promise(async (resolve, reject) => {
         try {
             const answers = await inquirer.prompt([
@@ -46,6 +50,26 @@ ClassStats.prototype.listChoice = function (variableName, text, option1, option2
     });
 };
 
+// this is a reusable funciton that creates a list basedo n 2 given options
+ClassStats.prototype.twoListChoices = function (variableName, text, option1, option2) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const answers = await inquirer.prompt([
+                {
+                    type: "list",
+                    name: `${variableName}`,
+                    message: `${text}`,
+                    choices: [`${option1}`, `${option2}`]
+                }
+            ]);
+            resolve(answers);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+// this pulls what occurs in the first choice and displays the correct string
 ClassStats.prototype.firstChoice = function (decision) {
     if (decision.firstChoice === "Right") {
         console.log("You fall down a steep hill, into a large lush forest");
@@ -58,10 +82,12 @@ ClassStats.prototype.firstChoice = function (decision) {
     }
 }
 
+// this runs if you die, pulling from the constructor
 ClassStats.prototype.death = function () {
     console.log(`Thank you for your adventure ${this.name}! You had ${this.money} when you died!`)
 }
 
+// this pulls what occurs from the orc decision and displays the correct line
 ClassStats.prototype.orcChoice = function (decision, value) {
     if (decision.firstOrc === "Run") {
         console.log("You get away safely, but trip down a hill in the process. You end up in the middle of a foreset.");
@@ -71,6 +97,30 @@ ClassStats.prototype.orcChoice = function (decision, value) {
     } else if (decision.firstOrc === "") {
         console.log("You are paralyzed with fear, the orc stares you down before beheading you.")
         value.death();
+    }
+}
+
+// this function is for the second main choice and displays the correct string
+ClassStats.prototype.secondChoice = function (decision, value) {
+    if (decision.goblin === "Fight" && this.strength >= 15) {
+        console.log("You defeat the goblin, you find 12 gold on his corpse. You gain 10 experience points");
+        this.money += 12;
+        this.experience += 10;
+    } else if (decision.goblin === "Fight" && this.strength < 15) {
+        console.log("You try your hardest, but utlimately are unsuccessful. You die!") 
+        value.death();
+    } else if (decision.goblin === "Run") {
+        console.log("You make the safe decision, you get away safely. You don't gain anything, but didn't die either.");
+    } else if (decision.goblin === "Hide" && this.type === "Archer") {
+        console.log("You wait for the right chance, the goblin turns his back on you and you unleash a shot from your bow.");
+        console.log("You find 12 gold and gain 10 experience.");
+        this.money += 12;
+        this.experience += 10;
+    } else if (decision.goblin === "Hide" && this.strength >= 15) {
+        console.log("You decide to hide, but realize that adventuring isn't for you. You decide to turn home while you still can");
+        console.log(`Thanks for playing ${this.name}! You have ${this.money} gold.`);
+    } else {
+        console.log("Error");
     }
 }
 
@@ -92,14 +142,19 @@ inquirer
         const storedValues = new ClassStats(data.name, data.class);
         storedValues.startingValues();
         storedValues.introduction();
-        storedValues.listChoice("firstChoice", "You come across a fork in the path, do you go left, right, or turn around?", "Left", "Right", "Turn Around")
+        storedValues.threeListChoice("firstChoice", "You come across a fork in the path, do you go left, right, or turn around?", "Left", "Right", "Turn Around")
             .then((pathAnswer) => {
                 storedValues.firstChoice(pathAnswer);
                 if (pathAnswer.firstChoice === "Left") {
-                    storedValues.listChoice("firstOrc", "What do you do?", "Run", "Fight", "Nothing")
+                    storedValues.threeListChoice("firstOrc", "What do you do?", "Run", "Fight", "Nothing")
                         .then((orcAnswer) => {
                             storedValues.orcChoice(orcAnswer, storedValues);
-                        })
-                }
+                        });
+                } 
+                storedValues.threeListChoice("goblin", "You see a goblin, what do you do?", "Run", "Fight", "Hide")
+                    .then((goblinAnswer) => {
+                        storedValues.secondChoice(goblinAnswer, storedValues);
+                    })
+
             })
 });
